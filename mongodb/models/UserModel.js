1aa91139e819dd3e8f4e3ec079bf7f9ffae9ccec
varis-crabroom <varis@crabroom.com>
2025-01-23 13:38:51 -0500

    Add userType field to UserModel with default value

--- mongodb/models/UserModel.js
+++ mongodb/models/UserModel.js
@@ -4,18 +4,19 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  email: { 
-    type: String, 
-    required: true, 
-    unique: true, 
-    match: [ /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, 'Please fill a valid email address' ] 
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, 'Please fill a valid email address']
   },
-  password: { 
-    type: String, 
-    required: true, 
+  password: {
+    type: String,
+    required: true,
   },
   token: { type: String },
   lastLogin: { type: Date },
+  userType: { type: String, default: "user" },
 }, { timestamps: true });
 
 const user = mongoose.models.user || mongoose.model("user", userSchema, "user");
